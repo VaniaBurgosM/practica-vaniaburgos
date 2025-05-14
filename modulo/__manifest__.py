85d7ce14d644bc83bbefdd4c02aecdc64b601b1c
Vania Burgos M <vania.burgos@sellside.cl>
2025-05-13 22:56:02 -0400

    commit - creación de vistas para producto y stock

--- modulo/__manifest__.py
+++ modulo/__manifest__.py
@@ -12,7 +12,8 @@
     'data': [
         'security/ir.model.access.csv',
         'views/acciones.xml',
-        'views/menus.xml',
+        'views/producto_views.xml',
+        'views/stock_operation_views.xml',
     ],
     'demo': [
         'demo/demo.xml',
