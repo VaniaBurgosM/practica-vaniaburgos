38fc8f305639d95eca638d321720193d95b37760
Vania Burgos M <vania.burgos@sellside.cl>
2025-05-18 22:58:09 -0400

    commit - pruebas

--- chatbot_gemini/models/agente_ia_gemini.py
+++ chatbot_gemini/models/agente_ia_gemini.py
@@ -17,17 +17,24 @@ class AgenteGemini(models.Model):
         result = super()._message_post_after_hook(message, msg_vals)
         bot_user = self.env.ref('chatbot_gemini.gemini_ai_user')
 
-        if message.author_id != bot_user.partner_id:
-            try:
-                self.with_user(bot_user)._handle_ai_response_gemini(message)
-            except Exception as e:
-                _logger.error(f"Error en _handle_ai_response_gemini: {e}")
+        # Verificación de unicidad del canal
+        if len(self) != 1:
+            _logger.error("self debe ser un único canal. Recibido: %s", self)
+            return result
 
-                self.with_user(bot_user).message_post(
-                    body="Ocurrió un error al procesar tu solicitud.",
-                    message_type='comment',
-                    subtype_xmlid='mail.mt_comment'
-                )
+        # Evita que el bot se responda a sí mismo
+        if message.author_id == bot_user.partner_id:
+            return result
+
+        try:
+            self.with_user(bot_user)._handle_ai_response_gemini(message)
+        except Exception as e:
+            _logger.error(f"Error en _handle_ai_response_gemini: {e}")
+            self.with_user(bot_user).message_post(
+                body="Ocurrió un error al procesar tu solicitud.",
+                message_type='comment',
+                subtype_xmlid='mail.mt_comment'
+            )
 
         return result
 
@@ -45,6 +52,7 @@ class AgenteGemini(models.Model):
             )
 
     def enviar_a_gemini(self, mensaje):
+        self.ensure_one()
         api_key = self.GEMINI_API_KEY
         api_url = self.GEMINI_API_URL + ":generateContent"
 
@@ -88,11 +96,14 @@ class AgenteGemini(models.Model):
             _logger.info(f"Respuesta Gemini: {response.text}")
             response.raise_for_status()
             response_json = response.json()
+        
             if 'candidates' in response_json and len(response_json['candidates']) > 0:
                 return response_json['candidates'][0]['content']['parts'][0]['text']
+        
             else:
                 _logger.warning("La API de Gemini devolvió una respuesta vacía.")
                 return "<p>La API de Gemini devolvió una respuesta vacía.</p>"
+        
         except requests.exceptions.RequestException as e:
             error_message = f"Error al comunicarse con la API de Gemini: {e}"
             _logger.error(error_message)
