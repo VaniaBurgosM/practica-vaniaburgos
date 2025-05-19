7ffb587ef35436ca3c7caf9218ca6317b2581bde
Vania Burgos M <vania.burgos@sellside.cl>
2025-05-19 14:52:57 -0400

    commit - prueba bot

--- chatbot_gemini/post_init.py
+++ chatbot_gemini/post_init.py
@@ -1,13 +1,18 @@
 from odoo import api, SUPERUSER_ID
 
 def post_init_hook(env):
-    group_public = env.ref('base.group_public')  # grupo público estándar de Odoo
+    group_public = env.ref('base.group_public')
 
     canal_model = env['discuss.channel']
-    canal = canal_model.search([('name', '=', 'Canal del Chatbot')], limit=1)
+    canal = canal_model.search([('name', '=', 'Asistente IA Gemini')], limit=1)
     if not canal:
         canal_model.create({
-            'name': 'Canal del Chatbot',
+            'name': 'Asistente IA Gemini',
             'channel_type': 'channel',
             'group_public_id': group_public.id,
+            'is_gemini_enabled': True,
         })
+
+    else:
+        if not canal.is_gemini_enabled:
+            canal.write({'is_gemini_enabled': True})
