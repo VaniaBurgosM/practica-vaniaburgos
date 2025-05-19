1bc575e53d89dceecd4e8dc65407e6776059c8ed
Vania Burgos M <vania.burgos@sellside.cl>
2025-05-19 14:12:50 -0400

    commit - herencia de vistas

--- chatbot_gemini/post_init.py
+++ chatbot_gemini/post_init.py
@@ -1,11 +1,13 @@
 from odoo import api, SUPERUSER_ID
 
 def post_init_hook(env):
+    group_public = env.ref('base.group_public')  # grupo público estándar de Odoo
+
     canal_model = env['discuss.channel']
     canal = canal_model.search([('name', '=', 'Canal del Chatbot')], limit=1)
     if not canal:
         canal_model.create({
             'name': 'Canal del Chatbot',
             'channel_type': 'channel',
-            'privacy': 'private',
+            'group_public_id': group_public.id,
         })
