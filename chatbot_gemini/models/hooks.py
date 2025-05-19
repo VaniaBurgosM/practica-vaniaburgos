e4fb92fecb052ac5710fdca4146b8b7736818ec4
Vania Burgos M <vania.burgos@sellside.cl>
2025-05-18 21:56:02 -0400

    commit - pruebas

--- chatbot_gemini/models/hooks.py
+++ chatbot_gemini/models/hooks.py
@@ -1,6 +1,8 @@
+from odoo import SUPERUSER_ID
+
 def set_password_post_init(cr, registry):
     from odoo.api import Environment
-    env = Environment(cr, 1, {})
-    user = env.ref('chatbot_gemini.gemini_ai_user')
+    env = Environment(cr, SUPERUSER_ID, {})
+    user = env.ref('chatbot_gemini.gemini_ai_user', raise_if_not_found=False)
     if user:
         user.sudo().write({'password': 'gemini_bot'})
